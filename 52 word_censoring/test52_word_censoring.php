e4e1b54acd86c012550c0c6fcdc3f9aad59f64e1
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-25 00:06:29 +0600

    Update test52_word_censoring.php

--- 52 word_censoring/test52_word_censoring.php
+++ 52 word_censoring/test52_word_censoring.php
@@ -6,7 +6,7 @@ if (isset($_POST['user_input']) && !empty($_POST['user_input'])) {
 
 <hr>
 
-<form action="test52%20word%20censoring.php" method="post">
+<form action="test52_word_censoring.php" method="post">
     <textarea name="user_input" cols="30" rows="6"><?php echo $user_input;?></textarea><br><br>
     <input type="submit" value="Submit">
 </form>
