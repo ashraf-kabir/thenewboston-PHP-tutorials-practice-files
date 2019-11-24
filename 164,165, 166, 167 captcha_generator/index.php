91167dc31d495df949b6725234db561399dabc9c
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-25 00:45:18 +0600

    changes

--- 164,165, 166, 167 captcha_generator/index.php
+++ 164,165, 166, 167 captcha_generator/index.php
@@ -12,9 +12,9 @@ if (!isset($_POST['secure'])) {
     }
 }
 
-$_SESSION['secure'] = rand(1000, 9999);
+//$_SESSION['secure'] = rand(1000, 9999);
 ?>
-<img src="generate.php"><br><br>
+<img src="generate.php"><br>
 
 <form action="index.php" method="post">
     Type the value you see:<input type="text" size="6" name="secure"> <input type="submit" value="submit">
